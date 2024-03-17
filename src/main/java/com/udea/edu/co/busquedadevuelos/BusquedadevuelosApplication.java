d282d2a4c636bc99690954014865bff71933bd62
JuanLema14 <andres.lema1@udea.edu.co>
2024-03-17 16:34:52 -0500

    Srping boot Initilizr

---
+++ src/main/java/com/udea/edu/co/busquedadevuelos/BusquedadevuelosApplication.java
@@ -0,0 +1,13 @@
+package com.udea.edu.co.busquedadevuelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusquedadevuelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusquedadevuelosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/edu/co/busquedadevuelos/BusquedadevuelosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.edu.co.busquedadevuelos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusquedadevuelosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
